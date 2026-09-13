a9895d7fe630192325915fe02d2dc8403fff7d13
amelielaw@live.co.uk <amelielaw@live.co.uk>
2026-09-13 19:14:42 +0000

    add notes to create_tables file

--- pipeline/01_create_tables.sql
+++ pipeline/01_create_tables.sql
@@ -1,4 +1,4 @@
-
+-- Create the schemas for each medallion layer of the pipeline
 CREATE CATALOG IF NOT EXISTS olist;
 CREATE SCHEMA IF NOT EXISTS olist.bronze;
 CREATE SCHEMA IF NOT EXISTS olist.silver;
